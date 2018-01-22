e14b735d685d89da836353aa7233227d306b7b09
ngesa marvin <marvinngesa@gmail.com>
2018-01-22 09:34:31 +0300

    register the GPS driver

--- app/src/main/java/com/droidmarvin/peripheraliointerfacinggpsperipheralusinguartgpsdriver/GpsService.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacinggpsperipheralusinguartgpsdriver/GpsService.java
@@ -3,8 +3,20 @@ package com.droidmarvin.peripheraliointerfacinggpsperipheralusinguartgpsdriver;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
+
+import com.google.android.things.contrib.driver.gps.NmeaGpsDriver;
+
+import java.io.IOException;
 
 public class GpsService extends Service {
+
+    public static final int UART_BAUD = 9600;
+    public static final float ACCURACY = 2.5f; // From GPS datasheet
+    private static final String TAG = GpsService.class.getSimpleName();
+    private static final String UART_BUS = "";
+    private NmeaGpsDriver mGpsDriver;
+
     public GpsService() {
     }
 
@@ -16,5 +28,15 @@ public class GpsService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         return START_NOT_STICKY;
+    }
+
+    private void setupGps() {
+        try {
+            // Register the GPS driver
+            mGpsDriver = new NmeaGpsDriver(this, UART_BUS, UART_BAUD, ACCURACY);
+            mGpsDriver.register();
+        } catch (IOException e) {
+            Log.w(TAG, "Unable to open GPS UART", e);
+        }
     }
 }
