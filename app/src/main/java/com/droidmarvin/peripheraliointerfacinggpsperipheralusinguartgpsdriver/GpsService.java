416588cf7dabab4fd6ca2b30d0b9f95f20f00d88
ngesa marvin <marvinngesa@gmail.com>
2018-01-22 09:19:06 +0300

    create onStartCommand

--- app/src/main/java/com/droidmarvin/peripheraliointerfacinggpsperipheralusinguartgpsdriver/GpsService.java
+++ app/src/main/java/com/droidmarvin/peripheraliointerfacinggpsperipheralusinguartgpsdriver/GpsService.java
@@ -10,7 +10,11 @@ public class GpsService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_NOT_STICKY;
     }
 }
